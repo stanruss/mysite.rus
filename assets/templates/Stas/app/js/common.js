c5d16e4ed052da50c073b57b08b8c577e6af7b8c
Stanruss <stan19781@gmail.com>
2018-01-22 15:13:14 +0300

    materializecss collections

--- assets/templates/Stas/app/js/common.js
+++ assets/templates/Stas/app/js/common.js
@@ -13,5 +13,34 @@ $( document ).ready(function(){
 	  	outDuration:400,
 	  	startingTop:'50%',
 	  });
+	$('.fixed-action-btn').openFAB();
+  $('.fixed-action-btn').closeFAB();
+  $('.fixed-action-btn.toolbar').openToolbar();
+  $('.fixed-action-btn.toolbar').closeToolbar();
+  $('.chips').material_chip();
+  $('.chips-initial').material_chip({
+    data: [{
+      tag: 'Apple',
+    }, {
+      tag: 'Microsoft',
+    }, {
+      tag: 'Google',
+    }],
+  });
+  $('.chips-placeholder').material_chip({
+    placeholder: 'Enter a tag',
+    secondaryPlaceholder: '+Tag',
+  });
+  $('.chips-autocomplete').material_chip({
+    autocompleteOptions: {
+      data: {
+        'Apple': null,
+        'Microsoft': null,
+        'Google': null
+      },
+      limit: Infinity,
+      minLength: 1
+    }
+  });
 })
 
